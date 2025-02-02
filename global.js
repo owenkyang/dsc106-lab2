0a5d644484627d80ac28951e3bd050b81bc7568b
Owen Yang <owentheyanger@gmail.com>
2025-02-01 16:40:19 -0800

    lab 4

--- global.js
+++ global.js
@@ -35,6 +35,7 @@ for (let p of pages) {
 
     nav.append(a);
 }
+
 document.body.insertAdjacentHTML(
     'afterbegin',
     `
@@ -51,7 +52,6 @@ document.body.insertAdjacentHTML(
 
 const themeSwitch = document.querySelector('#theme-switch');
 
-
 function setColorScheme(colorScheme) {
     document.documentElement.style.setProperty('color-scheme', colorScheme);
     localStorage.setItem('colorScheme', colorScheme); 
@@ -63,8 +63,61 @@ themeSwitch.addEventListener('input', (event) => {
     setColorScheme(selectedTheme);
 });
 
-
 document.addEventListener('DOMContentLoaded', () => {
     const savedTheme = localStorage.getItem('colorScheme') || 'auto';
     setColorScheme(savedTheme);
 });
+
+
+
+export async function fetchJSON(url) {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+
+            throw new Error(`Failed to fetch projects: ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching or parsing JSON data:', error);
+ 
+        throw error;
+    }
+}
+
+export function renderProjects(projects, containerElement, headingLevel = 'h2') {
+
+    if (!containerElement) {
+        console.error("Invalid container element provided for rendering projects.");
+        return;
+    }
+
+    containerElement.innerHTML = '';
+
+    if (!projects || projects.length === 0) {
+        containerElement.innerHTML = '<p>No projects available. Check back later!</p>';
+        return;
+    }
+
+    projects.forEach(project => {
+        let article = document.createElement('article');
+
+        let heading = document.createElement(headingLevel);
+        heading.textContent = project.title;
+
+        let image = document.createElement('img');
+        image.src = project.image;
+        image.alt = project.title;
+
+        let description = document.createElement('p');
+        description.textContent = project.description;
+        article.append(heading, image, description);
+
+        containerElement.appendChild(article);
+    });
+}
+export async function fetchGitHubData(username) {
+    return fetchJSON(`https://api.github.com/users/${username}`);
+}
